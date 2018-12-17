e112989f54435294e7c60bb39d3a23b557c4d00c
Jonathan Silverblood <gitlab@monsterbitar.se>
2018-12-17 19:54:17 +0000

    configure fetch parameters.

--- www/js/index.js
+++ www/js/index.js
@@ -19,14 +19,14 @@ const postData = function(url = ``, data = {})
 	// Default options are marked with *
 	return fetch(url, {
 		method: "POST", // *GET, POST, PUT, DELETE, etc.
-		mode: "same-origin", // no-cors, cors, *same-origin
+		mode: "no-cors", // no-cors, cors, *same-origin
 		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-		credentials: "same-origin", // include, *same-origin, omit
+		credentials: "omit", // include, *same-origin, omit
 		headers: {
 			"Content-Type": "application/json; charset=utf-8",
 			// "Content-Type": "application/x-www-form-urlencoded",
 		},
-		redirect: "follow", // manual, *follow, error
+		redirect: "error", // manual, *follow, error
 		referrer: "no-referrer", // no-referrer, *client
 		body: JSON.stringify(data), // body data type must match "Content-Type" header
 	})
